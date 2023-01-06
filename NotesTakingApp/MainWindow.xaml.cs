f61d1432728a94fb6404662017877ba7a19096b5
ArmandNt <rossouma@gmail.com>
2023-01-05 23:46:00 -0500

    Initial Commit

---
+++ NotesTakingApp/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NotesTakingApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ObservableCollection<Note> notes = new ObservableCollection<Note>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+        }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            clearNoteContent();
+        }
+
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            if (String.IsNullOrEmpty(txtboxTitle.Text))
+            {
+                MessageBox.Show("Enter a Title!");
+            }
+            else
+            {
+                if (String.IsNullOrEmpty(txtboxNote.Text))
+                {
+                    MessageBox.Show("Can't Save An Empty Note ☹️, Type Something!");
+                }
+                else
+                {
+                    notes.Add(new Note(txtboxTitle.Text, txtboxNote.Text));
+                    listView.Items.Add(txtboxTitle.Text + " : " + txtboxNote.Text);
+                    clearNoteContent();
+                }
+            }
+        }
+
+        public void clearNoteContent()
+        {
+            txtboxTitle.Text = "";
+            txtboxNote.Text = "";
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            //listView.Items.Remove(listView.SelectedItems);
+            //listView.Items.Refresh();
+        }
+
+        private void btnEdit_Click(object sender, RoutedEventArgs e)
+        {
+            //Note noteSelected = listView.SelectedItems as Note;
+        }
+    }
+}
---
+++ NotesTakingApp/Note.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotesTakingApp
+{
+    internal class Note
+    {
+
+        public string title { get; set; }
+        public string notes { get; set; }
+
+        public Note(string title, string note)
+        {
+            this.title = title;
+            this.notes = note;
+        }
+    }
+}
